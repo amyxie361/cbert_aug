4438b78a181ff839bd59459531e4d834424d2ec2
amyxie361 <amyxie361@outlook.com>
2019-10-15 11:44:28 -0400

    runable

--- text_classification/nlp_utils.py
+++ text_classification/nlp_utils.py
@@ -68,15 +68,16 @@ def convert_seq(batch, device=None, with_label=True):
     def to_device_batch(batch):
         if device is None:
             return batch
-        elif device < 0:
-            return [chainer.dataset.to_device(device, x) for x in batch]
         else:
-            xp = cuda.cupy.get_array_module(*batch)
-            concat = xp.concatenate(batch, axis=0)
-            sections = numpy.cumsum([len(x) for x in batch[:-1]], dtype='i')
-            concat_dev = chainer.dataset.to_device(device, concat)
-            batch_dev = cuda.cupy.split(concat_dev, sections)
-            return batch_dev
+            if type(device) is not int or device < 0:
+                return [chainer.dataset.to_device(device, x) for x in batch]
+            else:
+                xp = cuda.cupy.get_array_module(*batch)
+                concat = xp.concatenate(batch, axis=0)
+                sections = numpy.cumsum([len(x) for x in batch[:-1]], dtype='i')
+                concat_dev = chainer.dataset.to_device(device, concat)
+                batch_dev = cuda.cupy.split(concat_dev, sections)
+                return batch_dev
 
     if with_label:
         return [to_device_batch([x for x, _ in batch]),
